8af12311646cb4a97fade8292b214d66d2d6dd2c
jkim1258 <67191817+DetouristCollective@users.noreply.github.com>
2024-10-20 18:56:38 -0500

    Add items and inventory

---
+++ cache/visual/v1/capture.js
@@ -0,0 +1,75 @@
+//Item 'DB'
+//In an key:value store instead of a real DB for now
+const ItemRegistry = {
+	"PP-PROTO-PLMB-1": {
+		"display_name": "Plumbo",
+		"item_description": "Everyone has a Plumbo in their home",
+		"source": "Watching Plumbo: How They Do It Special in 2015",
+		"item_icon": "PLMB-1.png",
+	},
+	"PP-PROTO-SCBK-1": {
+		"display_name": "Shrude Buck",
+		"item_description": "Exchangeable for Stan's Nickels at the local beet farm",
+		"source": "Finding a hidden Shrude Buck in the 'Workplace' flash game in 2016",
+		"item_icon": "SCBK-1.png",
+	},
+	"PP-PROTO-PKST-1": {
+		"display_name": "Pink Stapler",
+		"item_description": "Belongs to Mildred. DO NOT STEAL",
+		"source": "Watching to the end of the credits of the movie 'Workspace' in 2017",
+		"item_icon": "PKST-1.png",
+	},
+	"PP-PROTO-PTPX-1": {
+		"display_name": "Platinum Pickaxe",
+		"item_description": "Pickaxe made from the most durable material in the Blockiverse",
+		"source": "Unlocking the Platinum Pickaxe achievement in 'Blockiverse' in 2012",
+		"item_icon": "PTPX-1.png",
+	},
+	"PP-PROTO-CMCB-1": {
+		"display_name": "Friend Cube",
+		"item_description": "The Friend Cube is a lie",
+		"source": "Completing level 4-5 without using the Friend Cube in 'Teleport Hole' in 2009",
+		"item_icon": "CMCB-1.png",
+	},
+	"PP-PROTO-ASNP-14": {
+		"display_name": "AmberStone National Park 2014",
+		"item_description": "2014 Edition of the AmberStone National Park Visitor Stamp",
+		"source": "Visiting AmberStone National Park in 2014",
+		"item_icon": "ASNP-14.png",
+	},
+	"PP-PROTO-RVNB-24": {
+		"display_name": "Revival Nut Butter 2024",
+		"item_description": "Great tasting nut butter that goes perfectly with the Jam",
+		"source": "Participating in the Revival Jam 2024",
+		"item_icon": "RVNB-24.png",
+	},
+	"PP-PROTO-RXVP-24": {
+		"display_name": "VisualCache Prototype",
+		"item_description": "First ever protype of the VisualCache",
+		"source": "Trying out the VisualCache Prototype in 2024",
+		"item_icon": "RXVP-24.png",
+	},
+	"PP-PROTO-DCSM-1": {
+		"display_name": "DeCo's Smile",
+		"item_description": "Reminder to smell more lemondates and make more roses, even when it's hard",
+		"source": "Reading through the end of the VisualCache description on itch.io in 2024",
+		"item_icon": "PLMB-2.png",
+	},
+};
+
+//Key "DB"
+//In an key:value store instead of a real DB for now
+const CodeRegistry = {
+	"temporary_raven_paddling_tabloid_trickily": "PP-PROTO-RVNB-24",
+	"temporary_elixir_ambiguity_patriot_overcrowd_helium": "PP-PROTO-RXVP-1",
+	"temporary_vocalize_voicing_chamber_craziness_trance": "PP-PROTO-DCSM-1",
+};
+
+const Inventory = [
+	"PP-PROTO-PLMB-1",
+	"PP-PROTO-SCBK-1",
+	"PP-PROTO-PKST-1",
+	"PP-PROTO-PTPX-1",
+	"PP-PROTO-CMCB-1",
+	"PP-PROTO-ASNP-14",
+];
