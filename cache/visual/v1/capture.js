b529873c3b1091f9bc8297757039e6d4a1ac2cfc
jkim1258 <67191817+DetouristCollective@users.noreply.github.com>
2024-10-20 22:29:27 -0500

    display assets

--- cache/visual/v1/capture.js
+++ cache/visual/v1/capture.js
@@ -17,67 +17,67 @@ const ItemRegistry = {
 		"display_name": "Pink Stapler",
 		"item_description": "Belongs to Mildred. DO NOT STEAL",
 		"source": "Watching to the end of the credits of the movie 'Workspace' in 2017",
-		"item_icon": "PKST-1.png",
+		"item_icon": "PKST-1.jpeg",
 	},
 	"PP-PROTO-PTPX-1": {
 		"display_name": "Platinum Pickaxe",
 		"item_description": "Pickaxe made from the most durable material in the Blockiverse",
 		"source": "Unlocking the Platinum Pickaxe achievement in 'Blockiverse' in 2012",
-		"item_icon": "PTPX-1.png",
+		"item_icon": "PTPX-1.jpeg",
 	},
 	"PP-PROTO-CMCB-1": {
 		"display_name": "Friend Cube",
 		"item_description": "The Friend Cube is a lie",
 		"source": "Completing level 4-5 without using the Friend Cube in 'Teleport Hole' in 2009",
-		"item_icon": "CMCB-1.png",
+		"item_icon": "CMCB-1.jpeg",
 	},
 	"PP-PROTO-ASNP-14": {
 		"display_name": "AmberStone National Park 2014",
 		"item_description": "2014 Edition of the AmberStone National Park Visitor Stamp",
 		"source": "Visiting AmberStone National Park in 2014",
-		"item_icon": "ASNP-14.png",
+		"item_icon": "ASNP-14.jpeg",
 	},
 	"PP-PROTO-RVNB-24": {
 		"display_name": "Revival Nut Butter 2024",
 		"item_description": "Great tasting nut butter that goes perfectly with the Jam",
 		"source": "Participating in the Revival Jam 2024",
-		"item_icon": "RVNB-24.png",
+		"item_icon": "RVNB-24.jpeg",
 	},
 	"PP-PROTO-RXVP-24": {
 		"display_name": "VisualCache Prototype",
 		"item_description": "First ever protype of the VisualCache",
 		"source": "Trying out the VisualCache Prototype in 2024",
-		"item_icon": "RXVP-24.png",
+		"item_icon": "VisualCache_Inverted_Transparent.png",
 	},
 	"PP-PROTO-DCSM-1": {
 		"display_name": "DeCo's Smile",
 		"item_description": "Reminder to smell more lemondates and make more roses, even when it's hard",
 		"source": "Reading through the end of the VisualCache description on itch.io in 2024",
-		"item_icon": "PLMB-2.png",
+		"item_icon": "DeCoIcon.png",
 	},
 	"PP-PROTO-MNLL-1": {
 		"display_name": "Monet Lily",
 		"item_description": "There's no debate that Lily is smiling in the painting :)",
 		"source": "Visiting the Lube Museum to see the Monet Lily in person",
-		"item_icon": "MNLL-1.png",
+		"item_icon": "MNLL-1.jpeg",
 	},
 	"PP-PROTO-LKPS-1": {
 		"display_name": "Lucky Purple Socks",
 		"item_description": "The same sock that the Governor is always wearing... Nobody knows why it was found at the local Orphanage",
 		"source": "Completing the Governor's character sidequest in 'Moonlit Field' in 2012",
-		"item_icon": "LKPS-1.png",
+		"item_icon": "LKPS-1.jpeg",
 	},
 	"PP-PROTO-GNEP-1": {
 		"display_name": "Giant Eggplant",
 		"item_description": "Our founding fathers, in their infinite wisdom, erected this statue at our capital to symbolize the importance of disseminating freedom and democracy",
 		"source": "Finding a hidden code near the Giant Eggplant Monument",
-		"item_icon": "GNEP-1.png",
+		"item_icon": "GNEP-1.jpeg",
 	},
 	"PP-PROTO-RKRL-1": {
 		"display_name": "Rick Rolls",
 		"item_description": "Never gonna give this joke up, never gonna let this habit down",
 		"source": "Finding the solution to a popular online treasure hunt published by fuzzbead.com",
-		"item_icon": "RKPRL-1.png",
+		"item_icon": "RKRL-1.png",
 	},
 };
 
@@ -112,10 +112,12 @@ function displayInventory() {
 		const item = ItemRegistry[itemCode];
 		const itemElement = document.createElement('div');
 		itemElement.innerHTML = `
+			<div class="collectible-icon-container">
+				<img class="collectible-icon" src="assets/${item.item_icon}" alt="${item.display_name}" />
+			</div>
 			<h2>${item.display_name}</h2>
 			<p>${item.item_description}</p>
 			<p><strong>Obtained by:</strong> ${item.source}</p>
-			<img src="${item.item_icon}" alt="${item.display_name}" />
 		`;
 		inventoryDiv.appendChild(itemElement);
 	});
